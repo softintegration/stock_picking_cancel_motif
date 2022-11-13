1cb307ec0de9544bd87631161f0b54ccef755069
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-13 10:10:37 +0100

    [FIX] fix the inheritance issues by using another method name for cancel with motif.

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -8,9 +8,6 @@ class StockPicking(models.Model):
     _name = 'stock.picking'
     _inherit = ['stock.picking', 'cancel.motif.class']
 
-    def action_cancel(self):
-        # Here we have to pay attention to pass the super method not the current or we will enter in endless vicious circle
-        return self.with_context(model=self._name,model_ids=self.ids,method='_origin_action_cancel')._action_cancel_motif_wizard()
+    def action_cancel_with_motif(self):
+        return self.with_context(model=self._name,model_ids=self.ids,method='action_cancel')._action_cancel_motif_wizard()
 
-    def _origin_action_cancel(self):
-        return super(StockPicking, self).action_cancel()
