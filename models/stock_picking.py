4b2e4e89b9704a6de3674fceebe10e7534e34d99
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-04-10 11:03:37 +0100

    [FIX] Picking:Save the cancel motif in the record

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -8,6 +8,13 @@ class StockPicking(models.Model):
     _name = 'stock.picking'
     _inherit = ['stock.picking', 'cancel.motif.class']
 
+    cancel_motif = fields.Text(string="Cancel motif",compute='_compute_cancel_motif',store=True)
+
+    @api.depends('cancel_motif_id')
+    def _compute_cancel_motif(self):
+        for each in self:
+            each.cancel_motif = each.cancel_motif_id.description or each.cancel_motif_id.name
+
     def action_cancel_with_motif(self):
         return self.with_context(model=self._name,model_ids=self.ids,method='action_cancel')._action_cancel_motif_wizard()
 
